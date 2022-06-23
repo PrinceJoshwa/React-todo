f4a8e668d3bb3aec85af7004faba112eade53365
PrinceJoshwa <sprincejoshwa85@gmail.com>
2022-06-23 10:29:00 +0530

    Updates

---
+++ precache-manifest.e8d0d6ac328293f08c45dea6d538eed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774b29a5f5d1b596a79c3bfd1912e58c",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "c300ca613f00ba6b9e1b",
+    "url": "/React-todo/static/css/main.66d783ee.chunk.css"
+  },
+  {
+    "revision": "56c33975a59bff775e01",
+    "url": "/React-todo/static/js/2.e21e66bc.chunk.js"
+  },
+  {
+    "revision": "c300ca613f00ba6b9e1b",
+    "url": "/React-todo/static/js/main.e596b66f.chunk.js"
+  },
+  {
+    "revision": "45aea7dcebb6519058d1",
+    "url": "/React-todo/static/js/runtime~main.d5f1644e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
